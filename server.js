d7167d2c3847573183359051fc8152b4ac098223
C'zar Wynn Dubois <czarwins@gmail.com>
2019-02-11 16:38:39 -0500

    trying to fix heroku

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use('/', routes)
 
 mongoose.connect(process.env.MONGODB_URI)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server is listening on PORT: ${PORT}`)
-})
+// const PORT = process.env.PORT || 3001
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on PORT: ${PORT}`)
+// })
+
+app.listen(process.env.PORT || 3001, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+ });
